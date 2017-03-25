6d6c610fa2fde9f549ab0a02622c7856a552823b
Fabio Serragnoli <fabio.serragnoli@sky.uk>
2017-03-25 17:19:53 +0000

    Added env configs

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,3 +1,4 @@
+import java.io.File
 import java.util.Date
 
 import akka.actor.ActorSystem
@@ -5,11 +6,12 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.io.StdIn
 
 
-object WebServer {
+object WebServer extends Configuration {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("my-hello-timed-world-system")
     implicit val materialiser = ActorMaterializer()
@@ -18,7 +20,7 @@ object WebServer {
     val route =
       path("hello-timed-world") {
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Hello Timed World: ${new Date}</h1>"))
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Hello Timed World: ${new Date}</h1>$secret. Port $serverPort"))
         }
       }
 
@@ -30,4 +32,20 @@ object WebServer {
       .flatMap(_.unbind())
       .onComplete(_ => system.terminate())
   }
+}
+
+object Configuration {
+  val env = System.getenv("ENV")
+
+  val config = env match {
+    case null => ConfigFactory.load()
+    case _ => ConfigFactory.load(env)
+  }
+}
+
+trait Configuration {
+  def config = Configuration.config
+
+  lazy val secret = config.getString("my.secret.value")
+  lazy val serverPort = config.getString("server.port")
 }
